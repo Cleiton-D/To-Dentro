553be3e1f1722198ae0ef5faecc8d0cdc3766101
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-12-03 22:18:45 -0400

    Fix components path location

--- src/pages/components/Logo/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Container } from './styles';
-
-const Logo = (): JSX.Element => <Container>Tô Dentro</Container>;
-
-export default Logo;
--- src/pages/components/Logo/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.h1`
-  font-family: Lobster;
-  font-size: 48px;
-  color: #4a86e8;
-`;
