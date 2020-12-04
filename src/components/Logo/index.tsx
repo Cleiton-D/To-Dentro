239ec5e9a151c2d10fc00d958816de198cae8a02
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-12-03 22:16:39 -0400

    Create logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from './styles';
+
+const Logo = (): JSX.Element => <Container>Tô Dentro</Container>;
+
+export default Logo;
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.h1`
+  font-family: Lobster;
+  font-size: 48px;
+  color: #4a86e8;
+`;
