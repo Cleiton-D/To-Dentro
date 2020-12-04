653f9c4755bd735be67f843c85b9508111a05cc1
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-12-03 22:50:26 -0400

    Add link to help page in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
+import { FiHelpCircle } from 'react-icons/fi';
+
 import Header from '../../components/Header';
 
-import { Container, WelcomeText } from './styles';
+import { Container, WelcomeText, AbouLink } from './styles';
 
 const Home = (): JSX.Element => {
   return (
@@ -8,6 +10,9 @@ const Home = (): JSX.Element => {
       <Header />
       <Container>
         <WelcomeText>Bem vindo</WelcomeText>
+        <AbouLink to="/help">
+          <FiHelpCircle color="#fff" size={24} />
+        </AbouLink>
       </Container>
     </>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.main`
   margin: 0 auto;
@@ -14,4 +16,25 @@ export const WelcomeText = styled.span`
   font-weight: 500;
   font-size: 32px;
   color: #4a86e8;
+`;
+
+export const AbouLink = styled(Link)`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  box-shadow: 0px 0px 10px #4e4e4e;
+  background-color: #4a86e8;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${shade(0.2, '#4a86e8')};
+  }
 `;
